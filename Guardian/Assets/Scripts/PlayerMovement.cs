29db67cc4235af65baa785c2c9e2ccb4ab2beece
Grayson Holt <gholtwork@gmail.com>
2022-03-22 19:59:19 -0500

    Added Minotaur Prefab to Player **Before trying to work with BlendTree
    Add the Minotaur as the player character, this is still set up with basic movement and jump and before trying to learn to animate with the BlendTree

--- Guardian/Assets/Scripts/PlayerMovement.cs
+++ Guardian/Assets/Scripts/PlayerMovement.cs
@@ -53,3 +53,61 @@ public class PlayerMovement : MonoBehaviour
     }
 
 }
+
+
+
+
+
+
+/*  Before trying to animate using the blend Tree
+
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpHeight = 2f;
+
+    Rigidbody rb;
+    Vector3 moveInput;
+    Vector3 playerMovement;
+    
+    float moveXPos;
+    float moveYPos;
+    float moveZPos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+
+    }
+
+
+    void Move()
+    {
+        moveXPos = moveInput.x * moveSpeed * Time.deltaTime;
+        moveZPos = moveInput.y * moveSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(moveXPos, 0f, moveZPos));
+    }
+
+
+    void OnMove(InputValue value)
+    {   
+        moveInput = value.Get<Vector2>();
+    }
+
+
+    void OnJump(InputValue value)
+    {
+        if(!value.isPressed) { return; }
+        
+        rb.velocity = new Vector3(0f, jumpHeight, 0f);
+        Debug.Log(value.Get());
+    }
+
+
+*/
