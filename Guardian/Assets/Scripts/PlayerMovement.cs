0fb6d02644d55bdcd31e3aa5c151fc26698d0f6b
Grayson Holt <gholtwork@gmail.com>
2022-03-22 23:32:40 -0500

    Forward and Backward movement animations

--- Guardian/Assets/Scripts/PlayerMovement.cs
+++ Guardian/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpHeight = 2f;
 
     Rigidbody rb;
+    Animator animator;
     Vector3 moveInput;
     Vector3 playerMovement;
     
@@ -16,25 +17,65 @@ public class PlayerMovement : MonoBehaviour
     float moveYPos;
     float moveZPos;
 
+    //Blendtree Variables
+    float locomotion; //BlendTree variable Locomotion
+    public float acceleration = 0.1f;
+    public float deceleration = 0.5f;
+    int locomotionHash;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+
+        //used for optimization purposes. Comparing two ints is faster than comparing two strings, used when comparing many animation states
+        locomotionHash = Animator.StringToHash("Locomotion");
     }
 
     
     void Update()
     {
         Move();
-
     }
 
 
     void Move()
     {
-        moveXPos = moveInput.x * moveSpeed * Time.deltaTime;
-        moveZPos = moveInput.y * moveSpeed * Time.deltaTime;
+        
+        //moveXPos = moveInput.x * moveSpeed * Time.deltaTime;
+        //moveZPos = moveInput.y * moveSpeed * Time.deltaTime;
 
-        transform.Translate(new Vector3(moveXPos, 0f, moveZPos));
+        
+        if(!Keyboard.current.wKey.isPressed && !Keyboard.current.sKey.isPressed)
+        {
+            if(locomotion >  .1f)
+            {
+                locomotion -= Time.deltaTime * deceleration;
+            }
+            else if(locomotion < -.1f)
+            {
+                locomotion += Time.deltaTime * acceleration;
+            }
+            else
+            {
+                locomotion = 0f;
+            }
+        }   
+
+        if(moveInput.y > 0f && locomotion < 2f) //RamRun animation is set to 2
+        {
+            locomotion += Time.deltaTime * acceleration;
+        }
+        else if(moveInput.y < 0f && locomotion > -1f) //WalkBackward animation is set to -1
+        {
+            locomotion -= Time.deltaTime * deceleration;
+        }
+
+
+        //transform.Translate(new Vector3(moveXPos, 0f, moveZPos));
+        
+        animator.SetFloat(locomotionHash, locomotion);
     }
 
 
@@ -53,9 +94,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
 }
-
-
-
 
 
 
