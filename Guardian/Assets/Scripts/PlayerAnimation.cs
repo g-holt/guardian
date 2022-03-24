f74e6639885a4e63508bf7ae40d5e399cb7fa7fc
Grayson Holt <gholtwork@gmail.com>
2022-03-23 22:06:12 -0500

    Animation Updates + Movement Added
    Forward and Backwards animation + movement Added

---
+++ Guardian/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Vector3 moveInput;
+    Animator animator;
+    
+    bool isSprinting;
+    float velocityMax = 1;
+
+    //Blend Tree Variables
+    int velocityHash;
+    public float velocity; //BlendTree variable Velocity
+    public float acceleration = 0.1f;
+    public float deceleration = 0.5f;
+
+
+    void Start()
+    {
+        velocityMax = 1f;
+
+        animator = GetComponentInChildren<Animator>();
+        //used for optimization purposes. Comparing two ints is faster than comparing two strings, used when comparing many animation states
+        velocityHash = Animator.StringToHash("Velocity");
+    }
+
+    
+    void Update()
+    {
+        AnimateMovement();
+    }
+
+
+    void AnimateMovement()
+    {
+        if (moveInput.y == 0)
+        {
+            SlowAnimation();
+        }
+
+        AnimateWalk();
+        animator.SetFloat(velocityHash, velocity);
+    }
+
+
+    void AnimateWalk()
+    {
+        if(!isSprinting && velocity > 1f) { StopSprint(); }
+
+        if (moveInput.y > 0f && velocity < velocityMax) //Walk animation is set to 2
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+        else if (moveInput.y < 0f && velocity > -1f) //WalkBackward animation is set to -1 
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+    }
+
+
+    void OnSprint(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            isSprinting = true;
+            velocityMax = 2f;
+        }
+        else if(!value.isPressed)
+        {
+            isSprinting = false;
+            velocityMax = 1f;
+        }
+    }
+
+
+    void StopSprint()
+    {
+        if(velocity > 1f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+        else
+        {
+            velocity = 1f;
+        }
+    }
+
+
+    void SlowAnimation()
+    {
+        if(velocity >  .01f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+        else if(velocity < -.01f)
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+        else
+        {
+            velocity = 0f;
+        }
+    }
+
+
+    void OnMove(InputValue value)
+    {   
+        moveInput = value.Get<Vector2>();
+    }
+
+}
--- Guardian/Assets/Scripts/PlayerMovement.cs
+++ Guardian/Assets/Scripts/PlayerMovement.cs
@@ -6,126 +6,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float jumpHeight = 2f;
 
     Rigidbody rb;
-    Animator animator;
     Vector3 moveInput;
     Vector3 playerMovement;
+    PlayerAnimation playerAnimation;
     
+    bool isMoving;
     float moveXPos;
     float moveYPos;
     float moveZPos;
-
-    //Blendtree Variables
-    float locomotion; //BlendTree variable Locomotion
-    public float acceleration = 0.1f;
-    public float deceleration = 0.5f;
-    int locomotionHash;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponentInChildren<Animator>();
-
-        //used for optimization purposes. Comparing two ints is faster than comparing two strings, used when comparing many animation states
-        locomotionHash = Animator.StringToHash("Locomotion");
+        playerAnimation = GetComponent<PlayerAnimation>();
     }
 
     
     void Update()
     {
         Move();
-    }
-
-
-    void Move()
-    {
-        if(!Keyboard.current.wKey.isPressed && !Keyboard.current.sKey.isPressed)
-        {
-            SlowMovement();
-        }   
-
-        if(moveInput.y > 0f && locomotion < 2f) //RamRun animation is set to 2
-        {
-            locomotion += Time.deltaTime * acceleration;
-        }
-        else if(moveInput.y < 0f && locomotion > -1f) //WalkBackward animation is set to -1
-        {
-            locomotion -= Time.deltaTime * deceleration;
-        }
-        
-        animator.SetFloat(locomotionHash, locomotion);
-    }
-
-
-    void SlowMovement()
-    {
-        if(locomotion >  .01f)
-        {
-            locomotion -= Time.deltaTime * deceleration;
-        }
-        else if(locomotion < -.01f)
-        {
-            locomotion += Time.deltaTime * acceleration;
-        }
-        else
-        {
-            locomotion = 0f;
-        }
-    }
-
-
-    // void OnMove(InputValue value)
-    // {   
-    //     moveInput = value.Get<Vector2>();
-    // }
-
-
-    void OnJump(InputValue value)
-    {
-        if(!value.isPressed) { return; }
-        
-        rb.velocity = new Vector3(0f, jumpHeight, 0f);
-        Debug.Log(value.Get());
-    }
-
-}
-
-
-
-/*  Before trying to animate using the blend Tree
-
-
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float jumpHeight = 2f;
-
-    Rigidbody rb;
-    Vector3 moveInput;
-    Vector3 playerMovement;
-    
-    float moveXPos;
-    float moveYPos;
-    float moveZPos;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    
-    void Update()
-    {
-        Move();
-
     }
 
 
     void Move()
     {
         moveXPos = moveInput.x * moveSpeed * Time.deltaTime;
-        moveZPos = moveInput.y * moveSpeed * Time.deltaTime;
+        moveZPos = playerAnimation.velocity * moveSpeed * Time.deltaTime;
 
         transform.Translate(new Vector3(moveXPos, 0f, moveZPos));
     }
@@ -136,14 +45,4 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
-
-    void OnJump(InputValue value)
-    {
-        if(!value.isPressed) { return; }
-        
-        rb.velocity = new Vector3(0f, jumpHeight, 0f);
-        Debug.Log(value.Get());
-    }
-
-
-*/
+}
